23a308302a1158862ebae462caf997e7a3a36c63
IsabelaOspina <isabela.ospinab@uqvirtual.edu.co>
2026-03-12 10:39:42 -0500

    creacion del repositorio

---
+++ src/main/groovy/org/example/gestionsolicitudes/GestionDeSolicitudesApplication.groovy
@@ -0,0 +1,13 @@
+package org.example.gestionsolicitudes
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class GestionDeSolicitudesApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(GestionDeSolicitudesApplication, args)
+    }
+
+}
---
+++ src/test/groovy/org/example/gestionsolicitudes/GestionDeSolicitudesApplicationTests.groovy
@@ -0,0 +1,13 @@
+package org.example.gestionsolicitudes
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GestionDeSolicitudesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
